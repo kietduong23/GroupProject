dd3f3e7d5dd72b47deedae6693829983309eb08e
KietDuong <108349102+kietduong23@users.noreply.github.com>
2023-09-10 22:24:09 +0700

    Update Customer.js

--- client/src/pages/Customer.js
+++ client/src/pages/Customer.js
@@ -56,7 +56,7 @@ function Customer() {
         setCategory('');
         setFilteredProducts([]);
     }
-    
+
     const handleLogout = () => handleCustomerLogout()
 
     return (
@@ -70,75 +70,80 @@ function Customer() {
             <div className='container'>{(user !== null) ? ('User: ' + user.email) : ('Using as guest')}</div>
             <div className="container">{(user !== null) ? (<button className='btn btn-primary' onClick={() => handleLogout()}>Log out</button>) : (<></>)}</div>
             <div className='container-fluid main'>
-            <div className='filter-box'>
-                <h3>Filter by category</h3>
-                <ul>
-                    {spans.map((span, index) => (
-                        <li key={index} id={span.id}
-                            onClick={() => handleSpanChange(span)}
-                            className={span.id == active ? active : 'deactive'}>{span.text}</li>
-                    ))}
-                </ul>
-            </div>
+                <div className='filter-box'>
+                    <h3>Filter by category</h3>
+                    <ul>
+                        {spans.map((span, index) => (
+                            <li key={index} id={span.id}
+                                onClick={() => handleSpanChange(span)}
+                                className={span.id == active ? active : 'deactive'}>{span.text}</li>
+                        ))}
+                    </ul>
+                </div>
 
-            {filteredProducts.length > 0 && (
-                    <div className='my-products'>
-                        <h2>Category: {category}</h2>
-                        <button className="btn btn-link" onClick={returntoAllProducts}>Return to All Products</button>
-                        <div className="container">
-                            <div className='products-box row row-cols-1 row-cols-sm-2 row-cols-lg-3'>
-                                {filteredProducts.map(product => (
-                                    <div className="p-3" key={product._id}>
-                                        <ProductCard className="col" product={product} />
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                )}
-                {filteredProducts.length < 1 && (
-                    <>
-                        {products.length > 0 && (
-                            <div className='my-products'>
-                                <h2>All Products</h2>
-                                <div className="container">
-                                    <div className='products-box row row-cols-1 row-cols-sm-2 row-cols-lg-3'>
-                                        {products.map(product => (
-                                            <div className="p-3" key={product.id}>
-                                                <ProductCard className="col" product={product} />
-                                            </div>
-                                        ))}
-                                    </div>
+                <div className='container search-box'>
+                    <input type="text" />
+                </div>
+
+                <div className='container all-products'>
+                    {filteredProducts.length > 0 && (
+                        <div className='my-products'>
+                            <h2>Category: {category}</h2>
+                            <button className="btn btn-link" onClick={returntoAllProducts}>Return to All Products</button>
+                            <div className="container">
+                                <div className='products-box row row-cols-1 row-cols-sm-2 row-cols-lg-3'>
+                                    {filteredProducts.map(product => (
+                                        <div className="p-3" key={product._id}>
+                                            <ProductCard className="col" product={product} />
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
-                        )}
-                        {products.length < 1 && (
-                            <div className='my-products please-wait'>Please wait...</div>
-                        )}
-                    </>
-                )}                
-            
-            </div>
+                        </div>
+                    )}
+                    {filteredProducts.length < 1 && (
+                        <>
+                            {products.length > 0 && (
+                                <div className='my-products'>
+                                    <h2>All Products</h2>
+                                    <div className="container">
+                                        <div className='products-box row row-cols-1 row-cols-sm-2 row-cols-lg-3'>
+                                            {products.map(product => (
+                                                <div className="p-3" key={product.id}>
+                                                    <ProductCard className="col" product={product} />
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                            {products.length < 1 && (
+                                <div className='my-products please-wait'>Please wait...</div>
+                            )}
+                        </>
+                    )}
 
-            <div className='container shopping-cart'>
-                <h2>My Shopping Cart</h2>
-                <div className='container'>
-                    <div>
-                        <ShoppingCart/>
+                </div>
+
+                <div className='container shopping-cart'>
+                    <h2>My Shopping Cart</h2>
+                    <div className='container'>
+                        <div>
+                            <ShoppingCart />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='container orders'>
-                <h2>My Order List</h2>
-                <div className='container'>
-                    <div>
-                        <OrderList/>
+                <div className='container orders'>
+                    <h2>My Order List</h2>
+                    <div className='container'>
+                        <div>
+                            <OrderList />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
 
