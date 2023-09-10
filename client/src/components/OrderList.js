362beb51d3cb74b96c3cb2b1b54c481580b3c326
KietDuong <108349102+kietduong23@users.noreply.github.com>
2023-09-10 22:18:34 +0700

    Update OrderList.js

--- client/src/components/OrderList.js
+++ client/src/components/OrderList.js
@@ -6,7 +6,7 @@ function OrderList() {
     const {orders} = useContext(CustomerContext);
     return (
         <div>
-            {orders.map((order) => <OrderCard items={order.cartItems} status={order.status}/>)}
+            {orders.map((order) => <OrderCard items={order.cartItems} status={order.status} order={order}/>)}
         </div>
     )
 }
