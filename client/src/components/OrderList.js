e12d428643101a8aa9a6018a826cafaa2ff42d4d
vinhhungnh <s3911246@rmit.edu.vn>
2023-09-11 14:08:10 +0700

    order ui update

--- client/src/components/OrderList.js
+++ client/src/components/OrderList.js
@@ -7,9 +7,11 @@ import '../css/order.css'
 function OrderList() {
     const {orders} = useContext(CustomerContext);
     return (
-        <div className='container-order'>
-            <div className='order-title'>My order list</div>
-            {orders.map((order) => <OrderCard key={order._id} items={order.cartItems} order={order}/>)}
+        <div className='container-cart'>
+            <div className='container cart-body'>
+                <div className='title'>My order list</div>
+                {orders.map((order) => <OrderCard key={order._id} items={order.cartItems} order={order}/>)}
+            </div>
         </div>
     )
 }
